77031c1b31a1949559089642a380892e3add3462
Abodawoud <yousefabodawoud@gmail.com>
2024-01-05 15:12:13 +0200

    classes

--- How_to_setup_project/dist/index.js
+++ How_to_setup_project/dist/index.js
@@ -1,2 +1,26 @@
 "use strict";
-console.log('Hello World');
+var _User_userId;
+class User {
+    constructor(name, email, age) {
+        this.city = "";
+        _User_userId.set(this, '');
+        this._name = name;
+        this._email = email;
+        this._age = age;
+    }
+}
+_User_userId = new WeakMap();
+const me = new User('Yousef', 'y@g.com', 21);
+// me.city = 'asd';
+// professional way
+class User2 {
+    constructor(name, email, age, city = '', userId = '') {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.city = city;
+        this.userId = userId;
+    }
+}
+const me2 = new User2('Yousef', 'y@g.com', 21);
+// me.city = 'asd';
--- How_to_setup_project/src/index.ts
+++ How_to_setup_project/src/index.ts
@@ -1,1 +1,32 @@
-console.log('Hello World');
+class User {
+  _name: string
+  _email: string
+  _age: number
+  readonly city: string = ""
+  #userId: string = ''
+
+  constructor(name: string, email: string, age: number) {
+    this._name = name;
+    this._email = email;
+    this._age = age;
+  }
+}
+const me = new User('Yousef', 'y@g.com', 21);
+// me.city = 'asd';
+
+
+// professional way
+class User2 {
+  constructor(
+    public name: string,
+    public email: string,
+    public age: number,
+    public readonly city: string = '',
+    // private userId: string
+  ) {
+  }
+}
+const me2 = new User2('Yousef', 'y@g.com', 21);
+// me.city = 'asd';
+
+// [protected userId: string] is only accessible inside the class and its children
